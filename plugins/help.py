303ad99f01c137d6813bf19243458db8e125ded7
ᏗᎮ๏ɭɭ๏「ᵀʰᵉ ˡᵒⁿᵉ ʷᵒˡᶠ」 <72794063+Apollothewolf@users.noreply.github.com>
2021-01-19 10:34:18 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"Heya!  It's a YouTube link To video/mp3 converter.currently bot supports single (no playlist). Check <a href='https://telegra.ph/How-to-use-MT-UtubeBot-01-19'>Here</a>"
     await message.reply_text(helptxt)
