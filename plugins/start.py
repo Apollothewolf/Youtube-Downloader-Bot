48fabb4d31faa796cb43a3e698361b5952470231
ᏗᎮ๏ɭɭ๏「ᵀʰᵉ ˡᵒⁿᵉ ʷᵒˡᶠ」 <72794063+Apollothewolf@users.noreply.github.com>
2021-01-19 11:28:56 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,8 +8,8 @@ async def start(client, message):
         [InlineKeyboardButton("©ⱮͲ_OᖴᖴIᑕIᗩᒪ™", url="https://t.me/TheRealApollo")],
        
     ])
-    welcomed = f"Heya <b>{message.from_user.first_name}</b>\nWelcome to @malluurlbot,\nThe Most Advanced utube Video and Audio Downloader in Telegram!\nplease send any YouTube url or hit /help for more info"
+    welcomed = f"Heya <b>{message.from_user.first_name}</b>\nWelcome to @malluurlbot,\nThe Most Advanced utube Video and Audio Downloader in Telegram!\nplease send any YouTube url or hit /help for more info\nPlease send any utube video link or search using @vid inline mode.\n "
 
-Please send any utube video link or search using @vid inline mode.\n "
+
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
