3b59c31b4734e34538486440e8f092443b4cb065
ᏗᎮ๏ɭɭ๏「ᵀʰᵉ ˡᵒⁿᵉ ʷᵒˡᶠ」 <72794063+Apollothewolf@users.noreply.github.com>
2021-01-19 22:20:58 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -4,5 +4,4 @@ from pyrogram import Client, Filters
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
     helptxt = f"Heya!  It's a YouTube link To video/mp3 converter.\ncurrently bot supports single (no playlist).\nCheck <a href='https://telegra.ph/How-to-use-MT-UtubeBot-01-19'>Here</a>"
-await message.disable_web_page_preview(True)
     await message.reply_text(helptxt)
